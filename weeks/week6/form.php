4e0a4dd71b51747269b5d4ad14c5230c3d215682
Isaac J <duckhunterjr@gmail.com>
2022-02-15 14:52:36 -0800

    add week 6 assign

---
+++ weeks/week6/form.php
@@ -0,0 +1,138 @@
+<?php
+$fNameErr = '';
+$lNameErr = '';
+$emailErr = '';
+$genderErr = '';
+$phoneErr = '';
+$winesErr = '';
+$regionsErr = '';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if(empty($_POST['fname'])) {
+        $fNameErr = 'Please enter your first name';
+    } else {
+        $fname = $_POST['fname'];
+    }
+
+    if(empty($_POST['lname'])) {
+        $lNameErr = 'Please enter your name';
+    } else {
+        $lname = $_POST['lname'];
+    }
+
+    if(empty($_POST['email'])) {
+        $emailErr = 'Please enter your Email';
+    } else {
+        $email = $_POST['email'];
+    }
+
+    if(empty($_POST['gender'])) {
+        $genderErr = 'Please choose your gender';
+    } else {
+        $gender = $_POST['email'];
+    }
+
+    if(empty($_POST['phone'])) {
+        $phoneErr = 'Please enter your phone number';
+    } else {
+        $phone = $_POST['phone'];
+    }
+
+    if(empty($_POST['wines'])) {
+        $winesErr = 'Please choose a wine';
+    } else {
+        $wines = $_POST['wines'];
+    }
+
+    if($_POST['regions'] == NULL) {
+        $regionsErr = 'Please select your regions';
+    } else {
+        $regions = $_POST['regions'];
+    }
+
+}// END server request
+
+?>
+
+<!-- Continue on video #2 21min mark -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/form.css" rel="stylesheet" type="text/css">
+    <title>Form Class Exercise</title>
+</head>
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+    <fieldset>
+    <legend>Contact Isaac</legend>
+    
+    <label>First Name</label>
+    <input type="text" name="fname" value="<?php if(isset($_POST['fname'])) echo htmlspecialchars($_POST['fname']);?>">
+    <span class="error"><?= $fNameErr?></span>
+
+    <label>Last Name</label>
+    <input type="text" name="lname" value="<?php if(isset($_POST['lname'])) echo htmlspecialchars($_POST['lname']);?>">
+    <span class="error"><?= $lNameErr?></span>
+    
+    <label>Email</label>
+    <input type="email" name="email" value="<?php if(isset($_POST['email'])) echo htmlspecialchars($_POST['email']);?>">
+    <span class="error"><?= $emailErr?></span>
+
+    <label>Gender</label>
+    
+    <ul>
+        <li><input type="radio" name="gender" value="female" <?php if(isset($_POST['gender'])&&$_POST['gender']=='female') echo 'checked = "checked" ' ?> >Female</li>
+
+        <li><input type="radio" name="gender" value="male" <?php if(isset($_POST['gender'])&&$_POST['gender']=='male') echo 'checked = "checked" ' ?> >Male</li>
+
+        <li><input type="radio" name="gender" value="neither" <?php if(isset($_POST['gender'])&&$_POST['gender']=='neither') echo 'checked = "checked" ' ?> >Neither</li>
+    </ul>
+    <span class="error"><?= $genderErr?></span>
+
+    <label>Phone</label>
+    <input type="tel" name="phone" value="<?php if(isset($_POST['phone'])) echo htmlspecialchars($_POST['phone']);?>">
+    <span class="error"><?= $phoneErr?></span>
+
+    <label>Favorite Wines</label>
+    
+    <ul>
+        <li><input type="checkbox" name="wines[]" value="cab" <?php if(isset($_POST['wines'])&& in_array('cab', $wines)) echo 'checked = "checked" ' ?> >Cabernet</li>
+
+        <li><input type="checkbox" name="wines[]" value="merlot" <?php if(isset($_POST['wines'])&& in_array('merlot', $wines)) echo 'checked = "checked" ' ?> >Merlot</li>
+
+        <li><input type="checkbox" name="wines[]" value="syrah" <?php if(isset($_POST['wines'])&& in_array('syrah', $wines)) echo 'checked = "checked" ' ?> >Syrah</li>
+
+        <li><input type="checkbox" name="wines[]" value="malbec" <?php if(isset($_POST['wines'])&& in_array('malbec', $wines)) echo 'checked = "checked" ' ?> >Malbec</li>
+    </ul>
+
+    <span class="error"><?= $winesErr?></span>
+
+    <label>Region</label>
+    <select name="regions">
+    <option value="" NULL <?php if(isset($_POST['regions'])&& $_POST['regions'] == NULL) echo 'selected = "unselected" ';?> >Select one</option>
+
+    <option value="nw" <?php if(isset($_POST['regions'])&& $_POST['regions'] == 'nw') echo 'selected = "selected" ';?> >Northwest</option>
+
+    <option value="sw" <?php if(isset($_POST['regions'])&& $_POST['regions'] == 'sw') echo 'selected = "selected" ';?> >Southwest</option>
+
+    <option value="mw" <?php if(isset($_POST['regions'])&& $_POST['regions'] == 'mw') echo 'selected = "selected" ';?> >Midwest</option>
+    
+    <option value="se" <?php if(isset($_POST['regions'])&& $_POST['regions'] == 'se') echo 'selected = "selected" ';?> >Southeast</option>
+
+    </select>
+
+    <span class="error"><?= $regionsErr?></span>
+
+    <input type="submit" value="Send it!">
+    </fieldset>
+    <p><a href="">Reset</a></p>
+    </form>
+    
+
+</body>
+</html>
