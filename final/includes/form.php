2307b7cdff74e19172e42acfc261c56af39b4d05
Isaac J <duckhunterjr@gmail.com>
2022-03-23 20:23:50 -0700

    changes email

--- final/includes/form.php
+++ final/includes/form.php
@@ -100,7 +100,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
              $_POST['venue'],
              $_POST['comments'],
              $_POST['privacy'])) {
-    $to = 'duckhunterjr@gmail.com';
+    $to = 'szemeo@mystudentswa.com';
     $subject = 'Test email '.date('m/d/y, h i A');
     $body = '
     First Name: '.$fname.' '.PHP_EOL.'
